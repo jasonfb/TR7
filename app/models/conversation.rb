7f468d048e1cce223fdb3012d0cf7f5704326770
Jason Fleetwood-Boldt <jason@heliosdev.shop>
2022-11-26 12:28:58 -0500

    Fixes for turbo

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,17 +1,5 @@
 class Conversation < ApplicationRecord
-
   include ActionView::RecordIdentifier
-
-  # after_update_commit lambda {
-  #   broadcast_replace_to self, target: "welcome__conversation__#{dom_id(self)}", partial: "welcome/conversation"
-  # }
-
-  after_update_commit lambda {
-    broadcast_replace_to self, target: "admin__#{dom_id(self)}", partial: "admin/conversations/line"
-  }
-
-  after_destroy_commit lambda {
-    broadcast_remove_to self, target: "admin__#{dom_id(self)}"
-
-  }
+  after_update_commit lambda { broadcast_replace_to self, target: "admin__#{dom_id(self)}", partial: "admin/conversations/line" }
+  after_destroy_commit lambda { broadcast_remove_to self, target: "admin__#{dom_id(self)}"}
 end
