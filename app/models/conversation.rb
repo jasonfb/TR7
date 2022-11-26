ff46ad757825075438df9b4349913751a9c699ab
Jason Fleetwood-Boldt <jason@heliosdev.shop>
2022-11-26 10:31:01 -0500

    turbo stream in two browsers

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -5,4 +5,9 @@ class Conversation < ApplicationRecord
   after_update_commit lambda {
     broadcast_replace_to self, target: "#{dom_id(self)}", partial: "welcome/conversation"
   }
+
+  after_update_commit lambda {
+    broadcast_replace_to self, target: "conversation__#{self.id}", partial: "admin/conversations/line"
+  }
+
 end
